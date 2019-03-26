ceb96869c7f48040e03df41aed7a0889f86cbfd0
DavidGG <hola@davidgg.es>
2019-03-26 13:00:07 +0100

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'todos',
+    component: TodoListComponent
+  },
+  {
+    path: 'posts',
+    component: PostListComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
   }
 ];
 
